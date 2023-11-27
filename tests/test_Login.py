2854086f014b5d6a94ad096a4e611cf28c4195ee
kumardevnotes <kumar.thoudishetti@gmail.com>
2023-11-27 20:23:37 +0530

    added all files

---
+++ tests/test_Login.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+import pytest
+from selenium.webdriver.common.by import By
+
+from pages.AccountPage import AccountPage
+from pages.HomePage import HomePage
+from pages.LoginPage import LoginPage
+from tests.BaseTest import BaseTest
+from utilities import ExcelUtils
+
+
+class TestSearch(BaseTest):
+
+    @pytest.mark.parametrize("email_address,password",ExcelUtils.get_data_from_excel("ExcelFiles/TestData.xlsx","LoginTest"))
+    def test_login_with_valid_credentials(self,email_address,password):
+        home_page = HomePage(self.driver)
+        login_page = home_page.navigate_to_login_page()
+        account_page = login_page.login_to_application(email_address,password)
+        assert account_page.display_status_of_edit_your_account_information_option()
+
+
+
+ def verify_Reports_With_Database(self,email_address,password):
+        home_page = HomePage(self.driver)
+        login_page = home_page.navigate_to_login_page()
+        account_page = login_page.login_to_application(email_address,password)
+        assert account_page.display_status_of_edit_your_account_information_option()
